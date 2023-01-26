cc7d69443fc65691d977d043b6ac5327999ffaa8
abubaker <codecookies958@gmail.com>
2023-01-26 23:59:32 +0500

    feature: implemented the increment and decrement array

--- pages/makeYourBurger.js
+++ pages/makeYourBurger.js
@@ -1,10 +1,12 @@
 import view from '../utils/view.js'
 import Ingredient from '../components/ingredient.js'
 import data from '../utils/data.js'
+import renderIngredientsData from '../utils/renderIngredientsData'
 
 export default function MakeYourBurger() {
-
+  const renderIngredientsArray = []
   const items = data.map((item) => Ingredient(item))
+
 
   view.innerHTML = `
  <div class="main__burger--grid">
@@ -52,21 +54,44 @@ export default function MakeYourBurger() {
     let btnIncrement = $increments[i].parentElement
     let btnDecrement = $decrements[i].parentElement
 
-    btnIncrement.addEventListener('click', () => handleIncrement(event, i))
+    btnIncrement.addEventListener('click', () => handleIncrement(Event, i))
 
-    btnDecrement.addEventListener('click', () => handleDecrement(event, i))
+    btnDecrement.addEventListener('click', () => handleDecrement(Event, i))
   }
 
-  function handleIncrement(event, i) {
+  function handleIncrement(Event, i) {
     const valueIncrement = $value[i]
     valueIncrement.innerText = parseInt(valueIncrement.innerText) + 1
+
+    if (valueIncrement.innerText > 2) {
+      valueIncrement.innerText = 2
+    }
+
+    const filterIngredients = renderIngredientsArray.filter(ingredient => ingredient.img === renderIngredientsData[i].img)
+    console.log(filterIngredients)
+
+    if (filterIngredients.length < 2) {
+      renderIngredientsArray.push(renderIngredientsData[i])
+      console.log(renderIngredientsArray)
+    }
   }
 
-  function handleDecrement(event, i) {
+  function handleDecrement(Event, i) {
     const valueDecrement = $value[i]
     valueDecrement.innerText = parseInt(valueDecrement.innerText) - 1
     if (valueDecrement.innerText < 0) {
       valueDecrement.innerText = 0
+    }
+    const filterIngredients = renderIngredientsArray.filter(ingredient => ingredient === renderIngredientsData[i].img)
+
+    function isIngredient() {
+      return filterIngredients.includes(renderIngredientsData[i].img)
+    }
+    console.log(isIngredient())
+    if (isIngredient()) {
+      const lastIndexOfIngredient = renderIngredientsArray.lastIndexOf(renderIngredientsData[i].img)
+      renderIngredientsArray.splice(lastIndexOfIngredient, 1)
+      console.log(renderIngredientsArray)
     }
   }
 }
---
+++ utils/renderIngredientsData.js
@@ -0,0 +1,25 @@
+export default [{
+        img: '../assests/renderItems/cutlet.svg'
+    },
+    {
+        img: '../assests/renderItems/mayo.svg'
+    },
+    {
+        img: '../assests/renderItems/onion.svg'
+    },
+    {
+        img: '../assests/renderItems/tomatoe.svg'
+    },
+    {
+        img: '../assests/renderItems/cucumber.svg'
+    },
+    {
+        img: '../assests/renderItems/cheese.svg'
+    },
+    {
+        img: '../assests/renderItems/salad.svg'
+    },
+    {
+        img: '../assests/renderItems/bun_top.svg'
+    },
+]
