27c19141d614551e93768bcce43e74171f13b178
abubaker <codecookies958@gmail.com>
2023-01-29 03:57:06 +0500

    modified the make burger btn link

--- pages/discovery.js
+++ pages/discovery.js
@@ -6,13 +6,11 @@ export default function Discovery() {
       <div class="title__screen--01">
         <h1>make your burger</h1>
       </div>
-      <p class="hero__paragraph--mob font-20 regular">Parallax screen. Burger ingredients and emojis moving
-        depending on
-        the position of
-        the
-        mouse pointer.</p>
+      <p class="hero__paragraph--mob font-20 regular">Parallax screen. Burger ingredients and emojis moving depending on the position of the
+      mouse pointer.
+      </p>
       <img class="hero__burger" src="./assests/main-img.svg" alt="hero-burger-image">
       <img class="hero__burger--mob" src="./assests/main-img-mob.svg" alt="hero-burger-image-mob">
-      <button class="btn btn--hero font-24 extra-bold "><span class="width__btn--hero">Make Burger</span></button>
+      <button class="btn btn--hero font-24 extra-bold "><span class="width__btn--hero"><a href="#/makeyourburger">Make Burger</a></span></button>
     </div>`
 }
